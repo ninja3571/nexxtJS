866fcbf596aedf9f0b27ec4444814a5fa240ade6
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2024-10-10 22:51:47 +0200

    1.7

--- zaj1/src/app/strona7/[kod]/page.js
+++ zaj1/src/app/strona7/[kod]/page.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react"
 import Image from "next/image"
-import Borders from "@/components/nextTo"
+import Borders from "@/components/borders"
 import Link from "next/link"
 
 export default function Info({params}){
@@ -30,30 +30,35 @@ export default function Info({params}){
     return(
         <div className='w-full h-3/4 flex justify-center mt-[60px]'>
             {load && <h1 className='font-bold'>Loading...</h1>}
-            {kraj.length>0 && <div className='m-[10px] h-[600px] w-[520px] bg-gradient-to-tl from-orange-300 to-rose-300 p-[7px] border-2 border-black'>
-                <div className='w-[400px] h-[300px] relative'>
+            {
+            kraj.length>0 && <div className='m-[10px] h-[750px] w-[520px] bg-gradient-to-tl from-orange-300 to-rose-300 p-[7px] border-2 border-black overflow-hidden'>
+                <div className='w-[400px] h-[300px] relative ml-[10%]'>
                     <Image
                     src={kraj[0].flags.png}
                     alt={kraj[0].name.common}
                     fill={true}
                     quality={100}
-                    priority></Image>
+                    priority
+                    objectFit="contain"></Image>
                 </div>
                 <div className='font-bold flex justify-center'>
                     <h1>{kraj[0].name.common}</h1>
                 </div>
                     <h1>STOLICA: {kraj[0].capital}</h1>
                     <h1>SHORT: {kraj[0].cca2}</h1>
-                    {/* borders */}
-                    <div>
-                        <h1>Borders:</h1>
-                        {kraj[0] && kraj.map((items, idx)=>(
-                            <Borders key={idx} kraje={items.borders}></Borders>
+                    <div className='font-bold flex justify-center mt-[20px]'>
+                    <h1>Sąsiedzi:</h1>
+                    </div>
+                    <div className="flex flex-row flex-wrap justify-center items-center gap-[3px]">
+                        
+                        {kraj[0] && kraj[0].borders.map((items, idx)=>(
+                            <Borders key={idx} data={items}></Borders>
                             ))
                         }
                         
                     </div>
-            </div>}
+            </div>
+            }
 
         </div>
     )
---
+++ zaj1/src/components/borders.js
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
+export default function Borders({data}){
+    
+    const [kraj, setKraj] = useState([])
+
+    useEffect(()=>{
+        const getData = async ()=>{
+             
+                try{  
+                    const resp = await fetch(`https://restcountries.com/v3.1/alpha/${data}`)
+                    const json = await resp.json()
+                    setKraj(json)
+                    console.log(json)
+
+                } catch(error){
+                    console.log(error)
+                } finally{
+                }
+            }
+
+        getData()    
+    }, [])
+
+    return(
+        <div>{kraj!=" " && 
+            
+            <Link href={`/strona7/${kraj.cca2}`}>
+                <div className="border-black border-2 w-[140px] h-[120px] flex flex-col items-center bg-gradient-to-bl from-lime-300 to-indigo-400 pt-[5px] ">
+                    <div className='w-[100px] h-[70px] relative'>
+                        {/* <Image
+                        src={kraj.flags.png}
+                        alt={kraj.name.common}
+                        fill={true}>
+                        </Image> */}
+                    </div>
+                    <h1>{kraj.name.common}</h1>
+                    <h3>stolica</h3>
+                </div>
+            </Link>
+            }
+        </div>
+
+
+    )
+}
