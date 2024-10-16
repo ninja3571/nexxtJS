6662fec7813542e747231eeafadf0eceba8ef24c
ninja3571 <125036855+ninja3571@users.noreply.github.com>
2024-10-16 21:34:07 +0200

    1.7

--- zaj1/src/app/strona4/page.js
+++ zaj1/src/app/strona4/page.js
@@ -3,20 +3,6 @@
 import { useState } from "react"
 
 export default function Page() {
-    // const [text, setText] = useState('')
-
-    // const zmien = ()=>{
-    //     const nowy = document.getElementById('nowyText').value
-    //     setText(nowy)
-    // }
-
-    // return(
-    //     <div>
-    //         <h1>{text}</h1>
-    //         <input type='text' id='nowyText' className="bg-indigo-600"></input>
-    //         <button onClick={zmien}>zmien</button>
-    //     </div>
-    // )
 
     const [text, setText] = useState('')
     const [input, setInput] = useState('')
